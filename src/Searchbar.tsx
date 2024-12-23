a38a32816821059a9e6fcf31259ad4327bed46b7
Phuc Tran <glup3.tran@gmail.com>
2024-12-23 19:44:38 +0100

    url has to start with https://www.willhaben.at/iad/immobilien

--- src/Searchbar.tsx
+++ src/Searchbar.tsx
@@ -4,8 +4,9 @@ import { useState } from "react"
 import { Advert, fetchData, toAdvert } from "./willhaben"
 
 export const Searchbar = (props: { setAdverts: (adverts: Advert[]) => void }) => {
-	const [text, setText] = useState("")
 	const { willhabenMap } = useMap()
+	const [text, setText] = useState("")
+	const [isInvalidUrl, setIsInvalidUrl] = useState(false)
 
 	const { mutate, isPending, isError } = useMutation({
 		mutationFn: (url: string) => fetchData(url),
@@ -15,15 +16,20 @@ export const Searchbar = (props: { setAdverts: (adverts: Advert[]) => void }) =>
 
 			const allCoords = adverts
 				.map((a) => ({ lat: a.latitude ?? 0, lng: a.longitude ?? 0 }))
-				.filter((a) => a.lat >= 47 && a.lat <= 48 && a.lng >= 9 && a.lng <= 10) // fix outliers that have sus coords
+				.filter((a) => a.lat >= 47.3 && a.lat <= 49.0 && a.lng >= 9.5 && a.lng <= 17.0) // fix outliers that have sus coords
 			const center = calculateCenter(allCoords)
 			willhabenMap?.flyTo({ center: [center[1], center[0]], zoom: 11 })
 		},
 	})
+
 	return (
 		<form
 			onSubmit={(e) => {
 				e.preventDefault()
+
+				if (isInvalidUrl) {
+					return
+				}
 
 				mutate(text)
 			}}
@@ -39,38 +45,56 @@ export const Searchbar = (props: { setAdverts: (adverts: Advert[]) => void }) =>
 					<input
 						name="willhaben-immo-url"
 						value={text}
-						onChange={(e) => setText(e.target.value)}
+						onChange={(e) => {
+							const newValue = e.target.value.trim()
+							setText(newValue)
+							setIsInvalidUrl(newValue !== "" && !newValue.startsWith("https://www.willhaben.at/iad/immobilien"))
+						}}
 						type="text"
 						placeholder="willhaben Immobilien URL einfügen. zb: https://www.willhaben.at/iad/immobilien/mietwohnungen/wien"
-						className="input input-bordered w-full focus:outline-none focus:ring-0"
+						className={[
+							"input input-bordered w-full focus:outline-none focus:ring-0",
+							isInvalidUrl ? "input-error" : "",
+						].join(" ")}
 						required={true}
 					/>
 				</label>
+
 				<button className="btn btn-primary" type="submit" disabled={isPending}>
 					{isPending && <span className="loading loading-spinner" />}
 					Suchen
 				</button>
 			</div>
 
+			{isInvalidUrl && (
+				<ErrorAlert text="Hoppla! Die URL scheint falsch zu sein. Sie sollte mit „https://www.willhaben.at/iad/immobilien“ beginnen." />
+			)}
+
 			{isError && (
-				<div role="alert" className="alert alert-error mt-4">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="h-6 w-6 shrink-0 stroke-current"
-						fill="none"
-						viewBox="0 0 24 24"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-						/>
-					</svg>
-					<span>Oje, das hat nicht funktioniert. Stimmt die URL oder hat sich ein kleiner Fehler eingeschlichen?</span>
-				</div>
+				<ErrorAlert text="Oje, das hat nicht funktioniert. Stimmt die URL oder hat sich ein kleiner Fehler eingeschlichen?" />
 			)}
 		</form>
+	)
+}
+
+const ErrorAlert = ({ text }: { text: string }) => {
+	return (
+		<div role="alert" className="alert alert-error mt-4">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="h-6 w-6 shrink-0 stroke-current"
+				fill="none"
+				viewBox="0 0 24 24"
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="2"
+					d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+				/>
+			</svg>
+			<span>{text}</span>
+		</div>
 	)
 }
 
